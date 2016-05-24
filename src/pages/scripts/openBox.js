ac24b5c197f7be59e5cd52a099266de4a00cdefc
Konstantin Mikhliuk <mikhliuk.k@gmail.com>
2016-05-24 14:01:26 +0300

    make deleting order button

---
+++ src/pages/scripts/openBox.js
@@ -0,0 +1,10 @@
+function openBox(id) {
+    var display = document.getElementById(id).style.display;
+
+    if (display == 'none') {
+        document.getElementById(id).style.display = 'block';
+    } else {
+        document.getElementById(id).style.display = 'none';
+    }
+    return false;
+}
