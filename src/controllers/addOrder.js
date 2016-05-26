72d9e8646fca46d530db8d90a4b705587b9f5815
Konstantin <konstantin.mihlyuk@yandex.ru>
2016-05-26 18:51:03 +0300

    subscribers

--- src/controllers/addOrder.js
+++ src/controllers/addOrder.js
@@ -1,25 +1,49 @@
-var OrdersModel = require('../models/order').model;
+var OrderModel = require('../models/order').model;
 var ServiceModel = require('../models/service').model;
 var moment = require('moment');
 
 module.exports = function (req, res, next) {
-    ServiceModel.findOne({_id: req.body.serviceId}, function (err, service) {
-        if (err) {
-            return next(err);
-        }
+    console.log('cart: ', req.session.cart);
+    
+    if (req.session.cart.isOwner == 'true') {
+        console.log('owner - true');
+        ServiceModel.findOne({_id: req.session.cart.serviceId}, addOwnerOrder);
+    } else {
+        console.log('owner - false');
+        OrderModel.findOne({_id: req.session.cart.orderId}, addSubscriberOrder);
+    }
 
-        var orderModel = new OrdersModel({
+    function addOwnerOrder(err, service) {
+        if (err) return next(err);
+
+        var orderModel = new OrderModel({
             owner: req.user,
-            orders: req.session.orders,
+            orders: req.session.cart.orders,
             service: service,
             creatingTime: moment(),
             subscriber: []
         });
-        
-        orderModel.save(function (err) {
-            req.session.orders = {};
+
+        orderModel.save(function () {
+            delete req.session.cart;
+            
+            res.redirect('/');
+        });
+    }
+
+    function addSubscriberOrder(err, order) {
+        if (err) next(new Error('Не создался подписчик'));
+
+        order.subscriber.push({
+            person: req.user,
+            orders: req.session.cart.orders,
+            paid: false
+        });
+
+        order.save(function (err) {
+            delete req.session.cart;
 
             res.redirect('/');
         });
-    });
+    }
 };
--- src/controllers/dishCountLess.js
+++ src/controllers/dishCountLess.js
@@ -1,7 +1,7 @@
 module.exports = function (req, res) {
     var removedDish = {};
     var needDelete = false;
-    req.session.orders.forEach(function (order, i, arr) {
+    req.session.cart.orders.forEach(function (order, i, arr) {
         if (order.dish._id == req.body.dishId) {
             removedDish = order.dish;
             if (order.count > 1) order.count = order.count - 1;
